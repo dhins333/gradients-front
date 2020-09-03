7e1f6488ac33c55ca29f61261eac7ad3e8dcea68
Dhinesh Kumar <dhins333@gmail.com>
2020-09-03 19:32:37 +0530

    404 page added

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -3,6 +3,7 @@ import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import Header from './components/Header';
 import MainPage from './components/MainPage';
 import ShowcasePage from './components/ShowcasePage';
+import ErrorPage from './components/404Page';
 
 const AppRouter = () => {
     return(
@@ -11,6 +12,7 @@ const AppRouter = () => {
             <Switch>
                 <Route path = '/' component = {MainPage} exact={true}></Route>
                 <Route path = '/showcase' component = {ShowcasePage} exact={true}></Route>
+                <Route path = '*' component = {ErrorPage}></Route>
             </Switch>
         </BrowserRouter>
     )
